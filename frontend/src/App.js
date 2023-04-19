d5cea2cfdcc0ba02575a431ae69a471a2041fb94
Sean Sarokas <sps4462@gmail.com>
2023-04-18 23:25:54 -0400

    creates frontend file structure

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from "react";
+
+function App() {
+
+  return (
+    <main>
+      <header>
+      <h1 className='title'>Team Tracker</h1>
+      </header>
+    </main>
+  );
+}
+
+export default App;
