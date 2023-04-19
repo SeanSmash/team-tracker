fd3aa178842ca5ebc76530fe888070c254b3a1f6
Sean Sarokas <sps4462@gmail.com>
2023-04-18 23:38:47 -0400

    creates teams and players components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import './App.css';
 import React from "react";
+import { Routes, Route, NavLink } from "react-router-dom";
+import Teams from './Teams';
+import Players from './Players';
 
 function App() {
 
@@ -8,6 +11,11 @@ function App() {
       <header>
       <h1 className='title'>Team Tracker</h1>
       </header>
+      <Routes>
+        <Route path="/teams" element={<Teams />} />
+        <Route path="/players" element={<Players />} />
+        {/* <Route path="*" element={<h1>404 not found</h1>} /> */}
+      </Routes>
     </main>
   );
 }
---
+++ frontend/src/Players.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Players(){
+
+    return(
+        <h1>Players</h1>
+    )
+}
+
+export default Players
